7df3729a4240cd77be2b2173bbfe6d2ffc6c3549
Saeed Kola <saeed@elementlabs.xyz>
2021-09-01 10:07:33 +0000

    Default warehouse fix

--- cefiro_customizations/public/js/purchase_receipt.js
+++ cefiro_customizations/public/js/purchase_receipt.js
@@ -4,34 +4,38 @@
 // });
 frappe.ui.form.on('Purchase Receipt',{
     get_items: function(frm,cdt,cdn){
-      frappe.call({
-        method: "cefiro_customizations.events.get_item_details_from_bundle_inserter",
-        args: {
-          bundle_list : frm.doc.product_bundle_inserter
-        },
-        callback: function(r){
-          // frm.set_value("items",[])
-          // var response = r.message
-          // for (const row in response){
-          //     // var itemsTable = frm.add_child("items");
-          //     // frappe.model.set_value(itemsTable.doctype, itemsTable.name, "item_code", row);
-          //     // frappe.model.set_value(itemsTable.doctype, itemsTable.name, "qty", response[row]);
-          //     items.push({
-          //       "item_code":row,
-          //       "qty": response[row]
-          //     })            
-          // } 
-          // var items = [];
-          // frm.set_value("items",[]);
-          // for (const row in r.message){
-          //   items.push(r.message[row]);
-          //   console.log(r.message[row]);
-          // }
-          frm.set_value("items",r.message);
-          frm.refresh_field("items");
-          // console.log(frm.doc.items);
-        }
-      });
+      if(frm.doc.set_warehouse && frm.doc.product_bundle_inserter){
+        set_default_warehouse(frm,cdt,cdn);
+        frappe.call({
+          method: "cefiro_customizations.events.get_item_details_from_bundle_inserter",
+          args: {
+            bundle_list : frm.doc.product_bundle_inserter,
+            set_warehouse: frm.doc.set_warehouse
+          },
+          callback: function(r){
+            // frm.set_value("items",[])
+            // var response = r.message
+            // for (const row in response){
+            //     // var itemsTable = frm.add_child("items");
+            //     // frappe.model.set_value(itemsTable.doctype, itemsTable.name, "item_code", row);
+            //     // frappe.model.set_value(itemsTable.doctype, itemsTable.name, "qty", response[row]);
+            //     items.push({
+            //       "item_code":row,
+            //       "qty": response[row]
+            //     })            
+            // } 
+            // var items = [];
+            // frm.set_value("items",[]);
+            // for (const row in r.message){
+            //   items.push(r.message[row]);
+            //   console.log(r.message[row]);
+            // }
+            frm.set_value("items",r.message);
+            frm.refresh_field("items");
+            // console.log(frm.doc.items);
+          }
+        });
+      }
     },
     refresh: function(frm,cdt,cdn){
       if(frm.doc.docstatus == 1){
@@ -43,17 +47,15 @@ frappe.ui.form.on('Purchase Receipt',{
         });
       }
     }
-    // ,
-    // validate: function(frm,cdt,cdn){
-    //   $.each(frm.doc.product_bundle_inserter||[],function(key,row){
-    //     if(!row.warehouse && frm.doc.set_warehouse){
-    //       console.log("True");
-    //       console.log(frm.doc.set_warehouse);
-    //       row.warehouse = frm.doc.set_warehouse;
-    //     }else{
-    //       console.log("false");
-    //     }
-    //   });
-    //   // frm.refresh_field("items");
-    // }
+    ,
+    validate: set_default_warehouse
 });
+
+function set_default_warehouse(frm,cdt,cdn){
+  $.each(frm.doc.product_bundle_inserter||[],function(key,row){
+    if(!row.warehouse && frm.doc.set_warehouse){        
+      row.warehouse = frm.doc.set_warehouse;
+    }
+  });
+  frm.refresh_field("product_bundle_inserter");
+}
